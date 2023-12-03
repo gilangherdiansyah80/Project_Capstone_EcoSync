cca016a5bf4886e22e1f27e05c3522891ff39511
gilangherdiansyah80 <exgbold@gmail.com>
2023-12-03 18:19:34 +0700

    Menghapus Import bootstrap  pada main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 import 'regenerator-runtime';
 import '../scss/styles.scss';
 import '../scss/responsive.scss';
-// eslint-disable-next-line no-unused-vars
-import * as bootstrap from 'bootstrap';
 import '../components/navbarComponent';
 import '../components/footerComponent';
 import '../components/head-custom';

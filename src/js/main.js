4215c9524aac999b9ae4f7c3f2ed071b994bc603
Arulkharisma <arulkharisma49@gmail.com>
2023-12-04 22:24:31 +0700

    add pagination for donation page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-loop-func */
 import 'regenerator-runtime';
 import '../scss/styles.scss';
 import '../scss/responsive.scss';
 import '../components/navbarComponent';
 import '../components/footerComponent';
 import '../components/head-custom';
+import Pagination from './pagination';
+
+const myPagination = new Pagination();
+myPagination.init();

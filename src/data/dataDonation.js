09d02c6d8314cb9369caf5657a4ca81c77554ed7
Arulkharisma <arulkharisma49@gmail.com>
2023-12-04 22:28:46 +0700

    add data donation file

---
+++ src/data/dataDonation.js
@@ -0,0 +1,60 @@
+const dataDonation = [
+  {
+    imageCard: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fd935839f-a443-43d8-abb1-9e684c08c00f.jpg%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75',
+    links: 'https://kitabisa.com/campaign/jagamasadepan',
+    title: 'Ikut Tanam 1 Pohon Tuk Selamatkan Masa Depan !',
+    description: 'Some quick example text to build on the card title and make up the bulk of the cardcontent',
+    imageSmall: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimg.kitabisa.cc%2Fsize%2F150x150%2F1db70bf8-8b53-4b1d-b180-9f492ed90d0c.jpg&w=64&q=75',
+    titleImageSmall: 'Hutan Itu Indonesiaku'
+  },
+  {
+    imageCard: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fd935839f-a443-43d8-abb1-9e684c08c00f.jpg%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75',
+    links: 'https://kitabisa.com/campaign/jagamasadepan',
+    title: 'Ikut Tanam 1 Pohon Tuk Selamatkan Masa Depan !',
+    description: 'Some quick example text to build on the card title and make up the bulk of the cardcontent',
+    imageSmall: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimg.kitabisa.cc%2Fsize%2F150x150%2F1db70bf8-8b53-4b1d-b180-9f492ed90d0c.jpg&w=64&q=75',
+    titleImageSmall: 'Hutan Itu Indonesiaku'
+  },
+  {
+    imageCard: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fd935839f-a443-43d8-abb1-9e684c08c00f.jpg%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75',
+    links: 'https://kitabisa.com/campaign/jagamasadepan',
+    title: 'Ikut Tanam 1 Pohon Tuk Selamatkan Masa Depan !',
+    description: 'Some quick example text to build on the card title and make up the bulk of the cardcontent',
+    imageSmall: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimg.kitabisa.cc%2Fsize%2F150x150%2F1db70bf8-8b53-4b1d-b180-9f492ed90d0c.jpg&w=64&q=75',
+    titleImageSmall: 'Hutan Itu Indonesiaku'
+  },
+  {
+    imageCard: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fd935839f-a443-43d8-abb1-9e684c08c00f.jpg%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75',
+    links: 'https://kitabisa.com/campaign/jagamasadepan',
+    title: 'Ikut Tanam 1 Pohon Tuk Selamatkan Masa Depan !',
+    description: 'Some quick example text to build on the card title and make up the bulk of the cardcontent',
+    imageSmall: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimg.kitabisa.cc%2Fsize%2F150x150%2F1db70bf8-8b53-4b1d-b180-9f492ed90d0c.jpg&w=64&q=75',
+    titleImageSmall: 'Hutan Itu Indonesiaku'
+  },
+  {
+    imageCard: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fd935839f-a443-43d8-abb1-9e684c08c00f.jpg%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75',
+    links: 'https://kitabisa.com/campaign/jagamasadepan',
+    title: 'Ikut Tanam 1 Pohon Tuk Selamatkan Masa Depan !',
+    description: 'Some quick example text to build on the card title and make up the bulk of the cardcontent',
+    imageSmall: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimg.kitabisa.cc%2Fsize%2F150x150%2F1db70bf8-8b53-4b1d-b180-9f492ed90d0c.jpg&w=64&q=75',
+    titleImageSmall: 'Hutan Itu Indonesiaku'
+  },
+  {
+    imageCard: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fd935839f-a443-43d8-abb1-9e684c08c00f.jpg%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75',
+    links: 'https://kitabisa.com/campaign/jagamasadepan',
+    title: 'Ikut Tanam 1 Pohon Tuk Selamatkan Masa Depan !',
+    description: 'Some quick example text to build on the card title and make up the bulk of the cardcontent',
+    imageSmall: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimg.kitabisa.cc%2Fsize%2F150x150%2F1db70bf8-8b53-4b1d-b180-9f492ed90d0c.jpg&w=64&q=75',
+    titleImageSmall: 'Hutan Itu Indonesiaku'
+  },
+  {
+    imageCard: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimgix.kitabisa.com%2Fd935839f-a443-43d8-abb1-9e684c08c00f.jpg%3Fauto%3Dformat%26fm%3Dpjpg%26ch%3DWidth%2CDPR%2CSave-Data%2CViewport-Width&w=640&q=75',
+    links: 'https://kitabisa.com/campaign/jagamasadepan',
+    title: 'Ikut Tanam 1 Pohon Tuk Selamatkan Masa Depan !',
+    description: 'Some quick example text to build on the card title and make up the bulk of the cardcontent',
+    imageSmall: 'https://kitabisa.com/_next/image?url=https%3A%2F%2Fimg.kitabisa.cc%2Fsize%2F150x150%2F1db70bf8-8b53-4b1d-b180-9f492ed90d0c.jpg&w=64&q=75',
+    titleImageSmall: 'Hutan Itu Indonesiaku'
+  },
+];
+
+export default dataDonation;

f29e5a1ace7fc04668704e1ded2d5c098cc8f1f7
Arulkharisma <arulkharisma49@gmail.com>
2023-12-18 00:31:57 +0700

    add tag meta name and delete folder fetch cuaca and bencana.html

--- src/components/head-custom.js
+++ src/components/head-custom.js
@@ -8,6 +8,7 @@ class headCustom extends HTMLElement {
             <head>
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta name="ecosync:email" content="arulkharisma01@gmail.com">
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&display=swap" rel="stylesheet">
